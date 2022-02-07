63a04c3f2e694de464bd8654ffedb54928f5a991
Kali Linux <nicholas.eden.long@gmail.com>
2022-02-07 22:40:46 +0000

    readme and install script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pen-test-environ
+
 pentesting environment
+
+# build and run
+
+Mounts your user git credentials into the docker for convenience.
+
+```
+./run.sh
+```
+
+# install in host
+
+```
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/tmux-scripts
-cp search-fzf-sploit.sh preview.sh ~/tmux-scripts
+cp parse-searchsploit-csv.sh search-fzf-sploit.sh preview.sh ~/tmux-scripts
 
 cp tmux.conf ~/.tmux.conf
 cp neovim-bindings.vim ~/.config/nvim/init.vim
