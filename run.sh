55b67b52248dd7b88ef47c70c678ea0e252953fd
Kali Linux <nicholas.eden.long@gmail.com>
2022-02-09 10:42:03 +0000

    dont run on failure to build

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker build . -t environ
-docker run --rm -it \
-  -v "$HOME/.git-credentials:/home/coyote/.git-credentials:ro" \
-  -v "$HOME/.gitconfig:/home/coyote/.gitconfig" \
-  -v "$(pwd):/host" \
-  -e "TERM=xterm-256color" \
-  environ
+if [ $? == 0 ]; then
+  docker run --rm -it \
+    -v "$HOME/.git-credentials:/home/coyote/.git-credentials:ro" \
+    -v "$HOME/.gitconfig:/home/coyote/.gitconfig" \
+    -v "$(pwd):/host" \
+    -e "TERM=xterm-256color" \
+    environ
+fi
