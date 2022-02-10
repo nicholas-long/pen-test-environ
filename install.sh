9a3d142687a695cc011975afb76e514b8a1b061b
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-10 00:02:42 +0000

    cleanup install script

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/tmux-scripts
-cp parse-searchsploit-csv.sh search-fzf-sploit.sh preview.sh ~/tmux-scripts
-
 cp tmux.conf ~/.tmux.conf
 cp neovim-bindings.vim ~/.config/nvim/init.vim
 
-#rm -rf ~/tmux-scripts
-cp -r tmux-scripts/* ~/tmux-scripts/
-
-cd ~/tmux-scripts && git clone https://github.com/dylanaraps/fff && git clone https://github.com/nicholas-long/github-exploit-code-repository-index
+mkdir -p ~/tmux-scripts
+cp -r tmux-scripts/* ~/tmux-scripts/ 2>/dev/null
+cd ~/tmux-scripts && git clone https://github.com/dylanaraps/fff 2>/dev/null && git clone https://github.com/nicholas-long/github-exploit-code-repository-index 2>/dev/null
 
