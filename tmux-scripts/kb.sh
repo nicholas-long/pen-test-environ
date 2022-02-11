e703d489a283fce00afebafca9862499cdfe57a6
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-11 06:51:44 +0000

    fix kb to use paging instead of trying to copy snippets

--- tmux-scripts/kb.sh
+++ tmux-scripts/kb.sh
@@ -9,7 +9,7 @@ fi
 
 getbyname () {
   read line file
-  tail -n +$line $file | $SCRIPT_DIR/get-markdown-snippet.py
+  tmux split-window -h "bat --paging=always $file"
 }
 
 grep -n -R '^#' $KB_DIR 2>/dev/null | grep -v '\.git' | grep -v '#!' | sed 's/:#\+/:/g' | fzf --delimiter ':' --nth=1,2 --with-nth=1,3 --preview="echo {} | sed 's/\(.*\):\([0-9]\+\):.*/\2: \1/g' | xargs -t bat --color=always -r" | awk -F ':' '{print $2, $1}' | getbyname
