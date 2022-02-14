c01e3d942f5cf8ca417bc280fe791785d290927e
nicholas-long <nicholas.eden.long@gmail.com>
2022-02-14 02:15:36 -0600

    this is sort of an install script to set up symlinks so development can continue

---
+++ symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s $(pwd)/functions.sh $HOME/functions.sh
+ln -s $(pwd)/tmux-scripts $HOME/tmux-scripts
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/neovim-bindings.vim $HOME/.config/nvim/init.vim
