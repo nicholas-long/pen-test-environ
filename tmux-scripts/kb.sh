eedab2dd7915a9ddbccd5128010af31687c3ea23
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-14 08:58:28 +0000

    this is better

--- tmux-scripts/kb.sh
+++ tmux-scripts/kb.sh
@@ -9,7 +9,7 @@ fi
 
 getbyname () {
   read line file
-  tmux split-window -h "bat --paging=always $file"
+  bat --color=always --paging=always --style=plain $file
 }
 
 grep -n -R '^#' $KB_DIR 2>/dev/null | grep -v '\.git' | grep -v '#!' | sed 's/:#\+/:/g' | fzf --delimiter ':' --nth=1,2 --with-nth=1,3 --preview="echo {} | sed 's/\(.*\):\([0-9]\+\):.*/\2: \1/g' | xargs -t bat --color=always -r" | awk -F ':' '{print $2, $1}' | getbyname
