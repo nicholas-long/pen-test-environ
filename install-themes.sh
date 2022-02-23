8ff6be2d182a73bf3827a4abbeec30e53ee603e9
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-23 23:43:36 +0000

    set up themes properly for zsh

--- install-themes.sh
+++ install-themes.sh
@@ -4,7 +4,5 @@ wget https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/master/gruvbox.zsh-the
 wget https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-# add the following to ~/.zshrc and uncomment
-#source /home/kali/functions.sh
-#source /home/kali/pen-test-environ/gruvbox.zsh-theme
-#source /home/kali/pen-test-environ/agnoster.zsh-theme
+# add to ~/.zshrc
+cat ~/tmux-scripts/kali-stuff-to-add-to-zsh.rc >> ~/.zshrc
