f4dc195183d65760dd052a0122d8c3ecce168907
Nicholas Long <nicholas.eden.long@gmail.com>
2022-02-23 17:43:08 +0000

    gruvbox scheme for tmux and terminal

--- symlinks.sh
+++ symlinks.sh
@@ -4,3 +4,4 @@ ln -s $(pwd)/functions.sh $HOME/functions.sh
 ln -s $(pwd)/tmux-scripts $HOME/tmux-scripts
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/neovim-bindings.vim $HOME/.config/nvim/init.vim
+ln -s $(pwd)/gruvbox.dircolors $HOME/.dircolors
