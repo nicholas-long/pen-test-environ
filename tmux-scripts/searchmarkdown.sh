98bc7abf2115abaa3280cf961fcc1a3708433d21
Nicholas Long <nicholas.eden.long@gmail.com>
2022-04-24 06:21:57 +0000

    search markdown relative paths

--- tmux-scripts/searchmarkdown.sh
+++ tmux-scripts/searchmarkdown.sh
@@ -72,9 +72,10 @@ getbyname () {
 }
 
 grep -n -R "$QUERY" "$KB_DIR" 2>/dev/null | \
+  sed "s.$KB_DIR/..g" | \
   $OPTIONAL_MD_SEARCH grep -v '\.git' | \
   grep -v '#!' | \
   sed 's/:#\+/:/g' | \
-  fzf --preview-window=$WINDOWLOC --delimiter ':' --nth=1,2 --with-nth=1,3 --preview="echo {} | sed 's/\(.*\):\([0-9]\+\):.*/\2: \1/g' | xargs -t bat --color=always -r" | \
-  awk -F ':' '{print $2, $1}' | \
+  fzf --preview-window=$WINDOWLOC --delimiter ':' --nth=1,2 --with-nth=1,3 --preview="echo {} | awk -F: '{ print \$2 \": $KB_DIR/\" \$1 }' | xargs -t bat --color=always -r" | \
+  awk -F ':' "{print \$2, \"$KB_DIR/\" \$1}" | \
   getbyname $paging
