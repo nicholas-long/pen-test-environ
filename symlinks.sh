5c67084f5e748681198e02f2293532e8859cfb0a
nicholas-long <nicholas.eden.long@gmail.com>
2022-07-29 11:43:51 -0500

    add alacritty config file to start fullscreen

--- symlinks.sh
+++ symlinks.sh
@@ -5,3 +5,4 @@ ln -s $(pwd)/tmux-scripts $HOME/tmux-scripts
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/neovim-bindings.vim $HOME/.config/nvim/init.vim
 ln -s $(pwd)/gruvbox.dircolors $HOME/.dircolors
+ln -s $(pwd)/alacritty.yml $HOME/.alacritty.yml
